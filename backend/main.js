2a637c0e6eded31537a4071deff14a179c73f6fd
Hizkias Abraham <hizkiasnew@gmail.com>
2022-12-28 12:49:02 +0300

    allowed cors request *

--- backend/main.js
+++ backend/main.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const { connectMongoDB } = require("./db");
 const { port } = require("./utils/constants");
 const athleteProfileRoute = require("./routes/athelete-profile");
 
 const app = express();
+app.use(cors({ origin: "*" }));
 app.use(bodyParser.json());
 app.use("/api/athlete-profile", athleteProfileRoute);
 
