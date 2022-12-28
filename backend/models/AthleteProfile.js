0cd2853c9c69130a2909ea04395bb1b736734a5e
Hizkias Abraham <hizkiasnew@gmail.com>
2022-12-28 12:49:02 +0300

    added AthleteProfile model

---
+++ backend/models/AthleteProfile.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const athleteProfileSchema = new mongoose.Schema({
+  name: {
+    type: String,
+  },
+  gender: {
+    type: String,
+  },
+  dob: {
+    type: String,
+  },
+  sport: {
+    type: String,
+  },
+  description: {
+    type: String,
+  },
+  location: {
+    type: String,
+  },
+  team: {
+    type: String,
+  },
+  profilePicture: {
+    type: String,
+  },
+});
+
+module.exports = mongoose.model("AthleteProfile", athleteProfileSchema);
