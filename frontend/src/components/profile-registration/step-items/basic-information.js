0946953f1e50619255727499b734c2485adc4581
Hizkias Abraham <hizkiasnew@gmail.com>
2022-12-28 12:49:02 +0300

    added sport autocomplete

--- frontend/src/components/profile-registration/step-items/basic-information.js
+++ frontend/src/components/profile-registration/step-items/basic-information.js
@@ -1,4 +1,5 @@
-import { Grid, TextField } from "@mui/material";
+import { Autocomplete, Grid, TextField } from "@mui/material";
+import { sports } from "../../../constants";
 
 export default function BasicInformation() {
   return (
@@ -7,7 +8,13 @@ export default function BasicInformation() {
         <TextField id="name" label="Name" variant="outlined" fullWidth />
       </Grid>
       <Grid item xs={12} md={6}>
-        <TextField id="sport" label="Sport" variant="outlined" fullWidth />
+        <Autocomplete
+          disablePortal
+          id="combo-box-demo"
+          fullWidth
+          options={sports}
+          renderInput={(params) => <TextField {...params} label="Sport" />}
+        />
       </Grid>
       <Grid item xs={12} md={6}>
         <TextField id="gender" label="Gender" variant="outlined" fullWidth />
