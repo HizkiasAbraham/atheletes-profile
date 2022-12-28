1d91424773341eda3a45e3cd7022170709dbaaba
Hizkias Abraham <hizkiasnew@gmail.com>
2022-12-28 12:49:02 +0300

    compeleted profile list

--- frontend/src/components/profile-list/index.js
+++ frontend/src/components/profile-list/index.js
@@ -1,3 +1,47 @@
+import { Box, Button, Divider, Typography } from "@mui/material";
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { getAllAtheleteProfiles } from "../../api";
+import Loading from "../loading";
+import ProfileListTable from "./table";
+
 export default function ProfileList() {
-  return <div>Profile List goes here</div>;
+  const [loading, setLoading] = React.useState(false);
+  const [data, setData] = React.useState([]);
+  const navigate = useNavigate();
+
+  const loadProfilesList = async () => {
+    setLoading(true);
+    try {
+      const result = await getAllAtheleteProfiles();
+      setData(result);
+      setLoading(false);
+    } catch (error) {}
+  };
+
+  React.useEffect(() => {
+    loadProfilesList();
+  }, []);
+
+  return (
+    <Box sx={{ width: "100%" }}>
+      <Typography variant="h5"> Athelete Profile List</Typography>
+      <Divider />
+      {loading && <Loading />}
+      {!loading && !data.length && (
+        <Box p={2} textAlign="center">
+          <Typography>No athlete profile aded</Typography>
+          <Button
+            onClick={() => {
+              navigate("/register-profile");
+            }}
+            variant="outlined"
+          >
+            Register Profile
+          </Button>
+        </Box>
+      )}
+      {!loading && !!data.length && <ProfileListTable data={data} />}
+    </Box>
+  );
 }
