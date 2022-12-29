3fa49d7491cd53dcab0df4a5d649da8990b20d2b
Hizkias Abraham <hizkiasnew@gmail.com>
2022-12-29 16:02:40 +0300

    added file upload and read

--- backend/main.js
+++ backend/main.js
@@ -5,14 +5,18 @@ const cors = require("cors");
 const { connectMongoDB } = require("./db");
 const { port } = require("./utils/constants");
 const athleteProfileRoute = require("./routes/athelete-profile");
+const filesRoute = require("./routes/files");
+const { setupGridfsStream } = require("./utils/setup-gridfs-stream");
 
 const app = express();
 app.use(cors({ origin: "*" }));
 app.use(bodyParser.json());
 app.use("/api/athlete-profile", athleteProfileRoute);
+app.use("/files", filesRoute);
 
 connectMongoDB()
   .then((_) => {
+    setupGridfsStream();
     app.listen(port, () => {
       console.log(`Server started listening on port: ${port}`);
     });
